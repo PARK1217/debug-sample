510515ca913b17701587b330b7516d08b56bd468
Susan <fasosan@gmail.com>
2022-07-12 13:02:33 +0900

    debug-sample

---
+++ src/main/java/org/example/util/Example02.java
@@ -0,0 +1,12 @@
+package org.example.util;
+
+public class Example02 {
+
+    public static void main(String[] args) {
+        int n = 0;
+        while (n < 10) {
+            n = Calculator.add(n, 1);
+        }
+        System.out.println("EXIT");
+    }
+}
